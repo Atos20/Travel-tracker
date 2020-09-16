c6c3d59b7bd12b9df075f2a7085bcc48baa59c94
Orlando Murcio <ato.murcio@gmail.com>
2020-09-16 15:52:54 -0600

    Add properties to user class

--- src/user.js
+++ src/user.js
@@ -1,7 +1,7 @@
 class User {
-    constructor(data) {
-        this.id = data.id;
-        this.name = data.name;
-    }
+  constructor(data) {
+      this.id = data.id;
+      this.name = data.name;
+  }
 }
 export default User;

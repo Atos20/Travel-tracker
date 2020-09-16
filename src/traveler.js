5cd40544275e11cfca69412d69f6173774373296
Orlando Murcio <ato.murcio@gmail.com>
2020-09-16 16:13:41 -0600

    Write test template for methods needed in traveler class

--- src/traveler.js
+++ src/traveler.js
@@ -1,11 +1,39 @@
 import User from './User'
+import moment from 'moment';
 class Traveler extends User {
   constructor(userInfo) {
     super(userInfo);
     // this.id = userInfo.id;
     // this.name = userInfo.name;
     this.type = userInfo.type;
-    this.tripsBooked = [];
+    this.tripsHistory = []; //past trips
+    this.currentTrip  = {} ; // || [] probably it will be just an object
+    this.upcomingTrips = [];
+    this.pendingTrips = [];
+    this.spentOverYear = 0;
+  }
+   //what format makes more sense?
+  //get trip history
+  getTripHistory() {
+   
+  }
+
+  //get currentTrip
+  getCurrentTrip() {
+
+  }
+  //get furtureTrips
+  getFutureTrips() {
+
+  }
+  //Get pending trips
+  getPendingTrips() {
+
+  }
+  //Total amount I have spent on trips this year
+  //claculate how much as a travler I have spent over the year
+  //This should be calculated from the trips data and include a travel agent’s 10% fee
+  spentOverTheYear() {
 
   }
 }

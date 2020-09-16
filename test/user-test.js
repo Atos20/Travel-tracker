d299b18a4c96f3242bd34ebf65cea7026fa60429
Orlando Murcio <ato.murcio@gmail.com>
2020-09-16 15:28:52 -0600

    Initialize test file

--- test/user-test.js
+++ test/user-test.js
@@ -2,13 +2,17 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import User from '../src/user.js';
+import testData from '../test-data/sample-data.js'
 
-let user;
+let user, user2, userData1, userData2;
 
 describe('User', () => {
-    beforeEach(() => {
-        user = new User();
-    });
+  beforeEach(() => {
+    userData1 = testData.userSampleData.travelers[0]
+    userData2 = testData.userSampleData.travelers[1]
+    user = new User(userData1);
+    user2 = new User(userData2);
+  });
   it('Should be able to be a function', () => {
     expect(User).to.be.a('function');
   });
@@ -16,4 +20,14 @@ describe('User', () => {
   it('Should be an instance of User', () => {
     expect(user).to.be.an.instanceof(User);
   });
+
+  it('Should be able to have an id', () => {
+      expect(user.id).to.equal(1)
+  });
+
+  it('Should be able to have an different id', () => {
+      expect(user2.id).to.equal(2)
+});
+
+
 });

9684dd842c5c2386a0150c32bd1c726550278d9d
Orlando Murcio <ato.murcio@gmail.com>
2020-09-16 18:33:30 -0600

    Set up the tripsRepo class

---
+++ src/tripsRepo.js
@@ -0,0 +1,14 @@
+class TripsRepo {
+    constructor(TripsData) {
+      this.tripsData = TripsData;
+    }
+
+    //get destinations by id
+    
+
+    // get destination cost
+
+
+  }
+
+  export default TripsRepo;

98008c27cbfca67793bac3ac9f117748c39806fe
Orlando Murcio <ato.murcio@gmail.com>
2020-09-17 17:18:22 -0600

    Add required parameter to assign travelers trip history

--- src/traveler.js
+++ src/traveler.js
@@ -1,21 +1,27 @@
 import User from './User'
+import TripsRepo from '../src/TripsRepo.js';
 import moment from 'moment';
 class Traveler extends User {
-  constructor(userInfo) {
+  constructor(userInfo, tripHistory) {
+    // console.log('I am in user=>',tripHistory)
     super(userInfo);
-    // this.id = userInfo.id;
-    // this.name = userInfo.name;
-    this.type = userInfo.type;
+    this.id = userInfo.id;
+    this.name = userInfo.name;
+    this.travelerType = userInfo.travelerType;
+    this.travelHistory = tripHistory.userTripHistory || [];
     this.tripsHistory = []; //past trips
     this.currentTrip  = {} ; // || [] probably it will be just an object
     this.upcomingTrips = [];
     this.pendingTrips = [];
     this.spentOverYear = 0;
+    // console.log(this.travelHistory)
   }
+
+
    //what format makes more sense?
   //get trip history
   getTripHistory() {
-   
+  //  console.log(this.travelHistory)
   }
 
   //get currentTrip

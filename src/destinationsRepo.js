dd6469ced52f418daeb90f3a37c09b44121e9976
Orlando Murcio <ato.murcio@gmail.com>
2020-09-18 19:09:24 -0600

    Write functionality to get cost per trip

--- src/destinationsRepo.js
+++ src/destinationsRepo.js
@@ -4,14 +4,24 @@ class DestinationsRepo {
     }
 
     //get destinations by id
-    getDestinationById(id) {
-      const destination = this.destinationsData.find( destination => destination.id === id);
-      console.log(destination)
+    getDestinationById(tripId) {
+      const destination = this.destinationsData.find( destination => destination.id === tripId);
+      // console.log(destination)
       return destination
     }
 
-    // get destination cost
-    
+    // get destination cost by id, days and people count
+    getDestinationCost(tripId, amountDays, amountPeople){
+      const trip = new Array(this.getDestinationById(tripId));
+      const totalCost = trip.reduce((total, trip) => {
+        const totalPerDay = trip.estimatedLodgingCostPerDay * amountDays;
+        const totalTripFlight = trip.estimatedFlightCostPerPerson * amountPeople;
+        total = totalPerDay + totalTripFlight;
+        return total;
+      }, 0)
+      // console.log( totalCost)
+      return  totalCost
+    }
 
   }
 
--- test/destinationsRepo-test.js
+++ test/destinationsRepo-test.js
@@ -22,4 +22,18 @@ describe('DestinationsRepo', () => {
   it.skip('Should be able to be an instance of DestinationsRepo class', () => {
     expect(newDestinationsRepo).to.be.an.instanceOf(DestinationsRepo);
   });
+
+  it.only('Should be able to get destinations by', () => {
+    const destination = newDestinationsRepo.getDestinationById(1)
+    expect(destination).to.eql(
+      {
+        "id": 1,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+        }
+    );
+  });
 });

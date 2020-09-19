b8610f949e345981ede38dd14b3878208dad82b3
Orlando Murcio <ato.murcio@gmail.com>
2020-09-18 23:53:35 -0600

    instantiate required classes on-start

--- src/index.js
+++ src/index.js
@@ -10,23 +10,31 @@ import './images/turing-logo.png'
 import Agent from '../src/agent.js';
 import Traveler from '../src/traveler.js';
 import TripsRepo from '../src/tripsRepo.js';
-import TravelerRepo from '../src/travelerRepo.js';
+import TravelersRepo from '../src/travelerRepo.js';
 import DestinationsRepo from '../src/destinationsRepo.js';
 import FecthHandler from '../src/fetchHandler.js'
 import DomUpdates from '../src/DomUpdates.js';
 
+let tripsRepo, destinationsRepo, travelersRepo, traveler, agent;
+
+
 const onStart = () => {
   let userId = (Math.floor(Math.random() * 49) + 1)
-  const allTravelersData = FecthHandler.getAllTravelersData();
-  const allDestinationsData = FecthHandler.getAllDestinationsData();
   const allTripsData = FecthHandler.getAllTripsData();
+  const allDestinationsData = FecthHandler.getAllDestinationsData();
+  const allTravelersData = FecthHandler.getAllTravelersData();
   const travelerData = FecthHandler.getSingleTravelerData(userId);
-  Promise.all([allTravelersData, allDestinationsData, allTripsData, travelerData])
-  .then(values => {
-    console.log(values)
-  })
 
+  Promise.all([allTripsData, allDestinationsData, allTravelersData, travelerData])
+  .then(values => {
+    tripsRepo = new TripsRepo(values[0]);
+    destinationsRepo = new DestinationsRepo(values[1]);
+    travelersRepo = new TravelersRepo(tripsRepo.historyByUserId(values[3].id));
+    traveler = new Traveler(values[3], travelersRepo, destinationsRepo.destinationsData);
+    console.log(traveler)
+  })
 }
+
 
 
 

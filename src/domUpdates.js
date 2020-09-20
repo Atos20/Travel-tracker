ddcc326c31e3d2fd55bb3fc9c3c6884155765bdf
Orlando Murcio <ato.murcio@gmail.com>
2020-09-19 19:55:20 -0600

    Disaplat gretting and amount per year

---
+++ src/domUpdates.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+
+const domUpdates = {
+
+  greetTraveler(traveler) {
+    const welcome = document.querySelector('.welcome');
+    const totalSpent = document.querySelector('.total-spent');
+    welcome.innerText = `Welcome ${traveler.name}!`;
+    totalSpent.innerText = `You have spent $${traveler.spentOverYearPlusFees}.00 so far`;
+  }
+
+
+
+}
+
+export default domUpdates;

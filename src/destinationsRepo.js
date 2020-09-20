44b3ec8f23f79a07eb0db657472861dd92a5a5bc
Orlando Murcio <ato.murcio@gmail.com>
2020-09-20 08:26:09 -0600

    Refactor get destination by property method

--- src/destinationsRepo.js
+++ src/destinationsRepo.js
@@ -3,16 +3,18 @@ class DestinationsRepo {
       this.destinationsData = destinationsData;
     }
 
-    // get destinations by id
-    getDestinationById(tripId) {
-      const destination = this.destinationsData.destinations.find( destination => destination.id === tripId);
+    // get destinations by id/name
+    getDestinationBy(spec, entry) {
+      const destination = this.destinationsData.destinations.find(destination => destination[spec] === entry);
       // console.log(destination)
       return destination
     }
 
+    
+
     // get destination cost by id, days and people count
     getDestinationCost(tripId, amountDays, amountPeople){
-      const trip = new Array(this.getDestinationById(tripId));
+      const trip = new Array(this.getDestinationBy('id', tripId));
       const totalCost = trip.reduce((total, trip) => {
         const totalPerDay = trip.estimatedLodgingCostPerDay * amountDays;
         const totalTripFlight = trip.estimatedFlightCostPerPerson * amountPeople;

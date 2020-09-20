ce1c35808bc5bb7574a35faa7a3dc007ca0b4a58
Orlando Murcio <ato.murcio@gmail.com>
2020-09-20 08:29:56 -0600

    Intsnatiate trip class from user form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
 import './css/base.scss';
 import './images/turing-logo.png'
-import animations from './animations.js';
+import moment from 'moment';
 
+import animations from './animations.js';
 import Agent from '../src/agent.js';
 import Traveler from '../src/traveler.js';
 import TripsRepo from '../src/tripsRepo.js';
@@ -10,12 +11,31 @@ import TravelersRepo from '../src/travelerRepo.js';
 import DestinationsRepo from '../src/destinationsRepo.js';
 import FecthHandler from '../src/fetchHandler.js'
 import domUpdates from '../src/DomUpdates.js';
+import Trip from './trip';
 
-let tripsRepo, destinationsRepo, travelersRepo, traveler, agent;
+let tripsRepo, destinationsRepo, travelersRepo, traveler, agent, newTrip;
 
 const mainMenu = document.querySelector('.hamburger');
 const newTripButton = document.querySelector('#world-globe');
 const submitNewTripButton  = document.querySelector('.submit');
+
+const submitNewTrip = (event) => {
+  const pickedDestination = document.querySelector('.destination-chosen');
+  const amountOfPeople = document.querySelector('.input1');
+  const date = document.querySelector('.input2');
+  const duration = document.querySelector('.input3');
+  event.preventDefault();
+  const userID = traveler.id;
+  const tripName = pickedDestination.value;
+  const travelers = amountOfPeople.value;
+  const tripDate = moment(date.value).format('YYYY/MM/DD');
+  const tripDuration = duration.value;
+  const destinationInfo = destinationsRepo.getDestinationBy('destination', tripName);
+  const destinationID = destinationInfo.id;
+  const desiredTrip = new Trip({userID, destinationID, travelers, tripDate , tripDuration});
+  //call for the function that will remove the form and display a message
+  //invoke the function that will POST the fetch request
+}
 
 const onStart = () => {
   let userId = (Math.floor(Math.random() * 49) + 1)
@@ -31,11 +51,11 @@ const onStart = () => {
     travelersRepo = new TravelersRepo(tripsRepo.historyByUserId(values[3].id));
     traveler = new Traveler(values[3], travelersRepo, destinationsRepo.destinationsData);
     domUpdates.greetTraveler(traveler)
-    // console.log(traveler)
+    // domUpdates.displayDestinations(destinationsRepo);
   })
 }
 
-submitNewTripButton.addEventListener('click', domUpdates.submitNewTrip)
+submitNewTripButton.addEventListener('click', submitNewTrip)
 newTripButton.addEventListener('click', domUpdates.toggleNewTripForm);
 mainMenu.addEventListener('click', animations.animateBurgerMenu);
 mainMenu.addEventListener('click', domUpdates.displayMenuOptions);

f2d11cb652ccd03859fe5239f646c825ba9fd6a7
Orlando Murcio <ato.murcio@gmail.com>
2020-09-20 06:13:58 -0600

    Dispalay new trip form

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,13 +2,24 @@ import moment from 'moment';
 
 const domUpdates = {
 
+  toggleNewTripForm(){
+    const newTripForm = document.querySelector('.form');
+    newTripForm.classList.remove('hidden');
+    console.log('form')
+
+  },
+
+  displayMenuOptions() {
+    const navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('hidden');
+  },
+
   greetTraveler(traveler) {
     const welcome = document.querySelector('.welcome');
     const totalSpent = document.querySelector('.total-spent');
     welcome.innerText = `Welcome ${traveler.name}!`;
-    totalSpent.innerText = `You have spent $${traveler.spentOverYearPlusFees}.00 so far`;
+    totalSpent.innerText = `You have spent $${traveler.spentOverYearPlusFees}.00 this Year`;
   }
-
 
 
 }
